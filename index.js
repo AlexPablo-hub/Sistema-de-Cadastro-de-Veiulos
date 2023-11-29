cf41b7844c7b46afe8ae33a09b54720cf44ba122
Alex Pablo de Oliveira Moraes <128164395+AlexPablo-hub@users.noreply.github.com>
2023-11-29 19:48:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
 }));
+
+// Importe o middleware express.urlencoded para análise de dados.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'ejs'); // Define EJS como mecanismo de template
+app.set('views', path.join(__dirname, 'views')); // Define o diretório de visualizações do aplicativo
+app.use(express.static(path.join(__dirname, 'HTML'))); // Define o diretório de arquivos estáticos do aplicativo.
+
+app.use(Routers);// Use o objeto de roteamento para todas as rotas
+
+// Inicia o servidor e faz com que ele escute a porta especificada.
+app.listen(port, (error) => {
+    if (error) {
+        console.log('O Servidor não está online, erro: ', error); // Em caso de erro ao iniciar o servidor, exibe uma mensagem de erro.
+        return;
+    }
+    console.log('O servidor está rodando na porta local: ' + port); // Se o servidor for iniciado com sucesso, exibe uma mensagem de sucesso.
+});
