9976b310378227e9fc1a2d92ed7a953b53e47a94
Alex Pablo de Oliveira Moraes <128164395+AlexPablo-hub@users.noreply.github.com>
2023-11-29 19:48:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const path = require('path'); // Importe o módulo de caminho do Node.js.
 const port = process.env.PORT || 3000; // Define a porta do servidor.
 const session = require('express-session'); // Importe o módulo de sessão do express.
 const Routers = require('./routers/Routers'); // Importe o arquivo de roteamento.
+
+// Configurações de sessão para o express.
+app.use(session({ 
+    secret: '84220084',
+    resave: false,
+    saveUninitialized: true,
+}));
